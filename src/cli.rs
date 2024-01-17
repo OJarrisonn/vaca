78c5b53e439fd43f8b9d5e7b55271c4881b298ea
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-17 16:24:19 -0300

    Created CLI definition for vaca

---
+++ src/cli.rs
@@ -0,0 +1,36 @@
+use clap::{Args, Subcommand};
+
+/// Vaca Lang repl, interpreter and compiler, all-in-one
+#[derive(Debug, clap::Parser)]
+#[command(name = "Vaca")]
+#[command(author = "OJarrisonn <j.h.m.t.v.10@gmail.com>")]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {
+    /// The command to be run
+    #[command(subcommand)]
+    pub command: Commands
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Starts a Vaca repl
+    Repl,
+    /// Runs a .vaca or .leite file
+    Run(RunArgs),
+    /// Builds a .vaca file an generates a .leite binary file
+    Build(BuildArgs)
+}
+
+#[derive(Args, Debug)]
+pub struct RunArgs {
+    pub file: String
+}
+
+#[derive(Args, Debug)]
+pub struct BuildArgs {
+    /// The input .vaca file to be built
+    pub input: String,
+    /// Optional .leite filename to store the build result
+    pub output: Option<String>
+}
