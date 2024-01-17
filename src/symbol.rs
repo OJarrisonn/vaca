490783647ef0561113b0d28aed4ff06ce09af3bb
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-16 23:32:32 -0300

    Created symbol with from implements

---
+++ src/symbol.rs
@@ -0,0 +1,22 @@
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct Symbol(String);
+
+impl From<&'static str> for Symbol {
+    fn from(value: &'static str) -> Self {
+        Self(String::from(value))
+    }
+}
+
+impl From<String> for Symbol {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
+impl Display for Symbol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "${}", &self.0)
+    }
+}
