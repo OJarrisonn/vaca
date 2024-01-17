60ad00f0a6eeb576b847c712e6d12de6f7633b42
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-16 23:31:48 -0300

    Defined data types for Vaca

--- src/data.rs
+++ src/data.rs
@@ -1,67 +1,76 @@
-use std::rc::Rc;
+use std::rc::Weak;
 
+use self::function::Function;
+
+pub mod function;
+pub mod owner;
+pub mod symbol_table;
+
+/// Vaca primitive data types, Data must be owned by the program Owner and it's weak references goes everywhere
+/// Once the scope where the data got created is destroyed, the data is freed
 #[derive(Debug)]
 pub enum Data {
+    Nil,
     Bool(bool),
     Integer(i64),
     Float(f64),
     Char(char),
     String(String),
-    Array(Vec<Rc<Data>>)
+    Array(Vec<Weak<Data>>),
+    Function(Function)
 }
 
-#[derive(Debug)]
-pub struct DataScope(Vec<Rc<Data>>);
-
-#[derive(Debug)]
-pub struct Owner {
-    owned: Vec<DataScope>
-}
-
-impl DataScope {
-    pub fn new() -> Self {
-        Self(vec![])
-    }
-}
-
-impl Owner {
-    pub fn new() -> Self {
-        Self {
-            owned: vec![]
+impl Into<Vec<Weak<Data>>> for Data {
+    fn into(self) -> Vec<Weak<Data>> {
+        match self {
+            Self::Array(a) => a,
+            d => panic!("Can't turn a {} into an array like", d)
         }
     }
+}
 
-    pub fn create_scope(&mut self) {
-        self.owned.push(DataScope::new());
-    }
-
-    pub fn drop_scope(&mut self) {
-        self.owned.pop();
-    }
-
-    pub fn insert(&mut self, data: Data) -> Rc<Data> {
-        { self.owned.last_mut().unwrap().0.push(Rc::new(data)); }
-
-        Rc::clone(self.owned.last().unwrap().0.last().unwrap())
+impl Data {
+    pub fn as_vec(&self) -> Vec<Weak<Data>> {
+        match self {
+            Self::Array(a) => a.clone(),
+            d => panic!("Can't turn a {} into an array like", d)
+        }
     }
 }
 
-#[test]
-fn data_test() {
-    let mut owner = Owner::new();
+#[cfg(test)]
+mod tests {
+    use crate::{Owner, Data, Function, Symbol};
 
-    owner.create_scope(); 
-    let data = owner.insert(Data::Bool(true));
+
+    #[test]
+    fn data_test() {
+        let mut owner = Owner::new();
+        
+        owner.create_scope(); 
+        let a1 = owner.insert(Data::Bool(true));
+        let a2 = owner.insert(Data::Integer(91));
+    let a3 = owner.insert(Data::String(String::from("Jorge")));
+    let a4 = owner.insert(Data::Char('b'));
+    
     let array = owner.insert(Data::Array(vec![
-        Rc::new(Data::Integer(7)),
-        Rc::new(Data::Integer(8)),
-        Rc::new(Data::Float(9.8)),
-        Rc::new(Data::Char('x')),
-    ]));
-
-    owner.drop_scope();
-
-    dbg!(owner);
-    dbg!(Rc::strong_count(&data));
-    dbg!(array);
+        a1, a2, a3, a4
+        ]));
+        
+        dbg!(&array.upgrade());
+        
+        owner.drop_scope();
+        
+        dbg!(&owner);
+        dbg!(&array.upgrade());
+    }
+    
+    #[test]
+    fn function_test() {
+        let mut owner = Owner::new();
+        
+        owner.create_scope();
+        
+        
+    }
 }
