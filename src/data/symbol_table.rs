245d257cd1779f66335f2ddf2ce54809ddd0ccbe
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-16 23:33:35 -0300

    Symbol table created with scoping

---
+++ src/data/symbol_table.rs
@@ -0,0 +1,45 @@
+use std::{rc::Weak, collections::{HashMap, LinkedList}};
+
+use crate::{Data, Symbol};
+
+#[derive(Debug)]
+pub struct SymbolScope(HashMap<Symbol, Weak<Data>>);
+
+pub struct SymbolTable {
+    scopes: LinkedList<SymbolScope>
+}
+
+impl SymbolScope {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+}
+
+impl SymbolTable {
+    pub fn new() -> Self {
+        Self {
+            scopes: LinkedList::new()
+        }
+    }
+
+    pub fn create_scope(&mut self) {
+        self.scopes.push_back(SymbolScope::new());
+    }
+
+    pub fn drop_scope(&mut self) {
+        self.scopes.pop_back();
+    }
+
+    pub fn insert(&mut self, symbol: Symbol, value: Weak<Data>) {
+        self.scopes.back_mut().unwrap().0.insert(symbol, value);
+    }
+
+    pub fn lookup(&self, symbol: &Symbol) -> Option<Weak<Data>> {
+        match self.scopes.iter()
+            .rfind(|scope| scope.0.contains_key(symbol)) {
+                Some(scope) => Some(Weak::clone(scope.0.get(symbol)?)),
+                None => None,
+            }
+            
+    }
+}
