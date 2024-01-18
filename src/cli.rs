414d79c45a497354a16c96ce50a69f85134af5b1
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-18 11:29:19 -0300

    Created settings parser for envconfig

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,5 @@
 use clap::{Args, Subcommand};
+use envconfig::Envconfig;
 
 /// Vaca Lang repl, interpreter and compiler, all-in-one
 #[derive(Debug, clap::Parser)]
@@ -34,3 +35,13 @@ pub struct BuildArgs {
     /// Optional .leite filename to store the build result
     pub output: Option<String>
 }
+
+#[derive(Envconfig, Debug)]
+pub struct Settings {
+    #[envconfig(from = "VACA_HOME")]
+    pub vaca_home: String,
+    #[envconfig(from = "VACA_REPL_HIST_LEN", default = "100")]
+    pub repl_history_len: usize,
+}
+
+// TODO Repl Helper
