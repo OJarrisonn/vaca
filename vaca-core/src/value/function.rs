f7faf8c5561e847525f369aae3f05d980f11074c
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-20 00:46:20 -0300

    Moved function definition to the right library

---
+++ vaca-core/src/value/function.rs
@@ -0,0 +1,74 @@
+use std::{iter::zip, rc::Rc};
+
+use crate::{Symbol, Value, SymbolTable, Form};
+
+#[derive(Debug, Clone)]
+pub struct Function {
+    arity: usize,
+    params: Vec<Symbol>,
+    partials: Vec<Rc<Value>>,
+    body: Option<Form>,
+    native: Option<NativeFunction>
+}
+
+/// The function call takes care of passing the return value to the previous ownership scope
+pub type NativeFunction = fn(&mut SymbolTable) -> Result<Rc<Value>, String>;
+
+impl Function {
+    pub fn new(params: Vec<Symbol>, body: Form) -> Self {
+        Self {
+            arity: params.len(), 
+            params,
+            partials: vec![],
+            body: Some(body),
+            native: None
+        }
+    }
+
+    pub fn native(params: Vec<Symbol>, native: NativeFunction) -> Self {
+        Self { 
+            arity: params.len(), 
+            params, 
+            partials: vec![],
+            body: None, 
+            native: Some(native) 
+        }
+    }
+
+    pub fn arity(&self) -> usize {
+        self.arity
+    }
+
+    pub fn exec(&self, source_args: Vec<Rc<Value>>, table: &mut SymbolTable) -> Result<Rc<Value>, String> {
+        if self.arity < source_args.len() {
+            return Err(format!("Missmatch on argument count, expected {}, got {}", self.arity, source_args.len()));
+        } else if self.arity > source_args.len() {
+            return Ok(Rc::new(Value::Function(Function::partial(&self, source_args))));
+        }
+
+        let mut args = self.partials.clone();
+        args.extend(source_args.into_iter());
+        
+        table.create_scope();
+
+        zip(&self.params, args).for_each(|(s, v)| table.register(s.clone(), v));
+
+        let res = if self.body.is_none() {
+            self.native.unwrap()(table)
+        } else {
+            self.body.as_ref().unwrap().eval(table)
+        };
+
+        table.drop_scope();
+
+        return res;
+    }
+
+    pub fn partial(source: &Self, args: Vec<Rc<Value>>) -> Self {
+        let mut source = source.clone();
+        source.arity -= args.len();
+        source.partials.extend(args.into_iter());
+
+        source
+    }
+}
