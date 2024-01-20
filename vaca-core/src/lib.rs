5d3d8c164034430df034374b2aa8af691086515f
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-20 00:20:35 -0300

    Created and exported value and symbol

--- vaca-core/src/lib.rs
+++ vaca-core/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//mod form;
+mod value;
+mod symbol;
+
+pub use crate::{value::Value, symbol::Symbol};
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    
 }
