e00c489cc24b3b0cc80bf28062b73d6f52ad5945
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-20 00:19:11 -0300

    Created modules for the vm

--- vaca-vm/src/lib.rs
+++ vaca-vm/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod table;
+
+pub use crate::table::SymbolTable;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    
 }
