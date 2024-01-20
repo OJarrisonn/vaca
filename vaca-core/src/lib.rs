2642de585697d7ca5a1dbbeb0dc0346b833e059b
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-20 00:45:23 -0300

    Exported lib modules

--- vaca-core/src/lib.rs
+++ vaca-core/src/lib.rs
@@ -1,8 +1,10 @@
 //mod form;
 mod value;
 mod symbol;
+mod table;
+mod form;
 
-pub use crate::{value::Value, symbol::Symbol};
+pub use crate::{value::Value, symbol::Symbol, table::SymbolTable, form::Form};
 
 #[cfg(test)]
 mod tests {
