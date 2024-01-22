73ba114d30ce9c234133ad4693ca2e987f929d1b
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-22 08:44:15 -0300

    Exporting new Error type

--- vaca-core/src/lib.rs
+++ vaca-core/src/lib.rs
@@ -7,7 +7,7 @@ mod error;
 #[macro_use]
 mod macros;
 
-pub use crate::{value::{Value, result::ExecResult}, symbol::Symbol, table::SymbolTable, form::Form, error::GenericError};
+pub use crate::{value::{Value, result::ExecResult}, symbol::Symbol, table::SymbolTable, form::Form, error::ErrorStack};
 
 #[cfg(test)]
 mod tests {
