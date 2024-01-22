35074bc40df3f688f60e5a8eec68182fc5b3b02a
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-22 08:23:53 -0300

    Created unused API for custom Result

---
+++ vaca-core/src/value/result.rs
@@ -0,0 +1,37 @@
+use std::{rc::Rc, ops::ControlFlow};
+
+use crate::Value;
+
+type Success = Rc<Value>;
+type Failure = String;
+pub enum ExecResult {
+    Success(Success),
+    Failure(Failure)
+}
+
+impl ExecResult {
+    pub fn unwrap(self) -> ControlFlow<Failure, Success> {
+        match self {
+            ExecResult::Success(s) => ControlFlow::Continue(s),
+            ExecResult::Failure(f) => ControlFlow::Break(f),
+        }
+    }
+}
+
+impl Into<Result<Success, Failure>> for ExecResult {
+    fn into(self) -> Result<Success, Failure> {
+        match self {
+            ExecResult::Success(s) => Ok(s),
+            ExecResult::Failure(f) => Err(f),
+        }
+    }
+}
+
+impl From<Result<Success, Failure>> for ExecResult {
+    fn from(value: Result<Success, Failure>) -> Self {
+        match value {
+            Ok(s) => ExecResult::Success(s),
+            Err(f) => ExecResult::Failure(f),
+        }
+    }
+}
