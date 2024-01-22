5e43935a85326d28e27cd23a35cfb9e25feb3978
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-22 08:21:36 -0300

    Exported future APIs

--- vaca-core/src/lib.rs
+++ vaca-core/src/lib.rs
@@ -7,7 +7,7 @@ mod error;
 #[macro_use]
 mod macros;
 
-pub use crate::{value::Value, symbol::Symbol, table::SymbolTable, form::Form, error::GenericError};
+pub use crate::{value::{Value, result::ExecResult}, symbol::Symbol, table::SymbolTable, form::Form, error::GenericError};
 
 #[cfg(test)]
 mod tests {
