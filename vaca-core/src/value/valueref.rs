b98b463548203f0230b8d30699ab54bce4af79e6
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-25 15:48:02 -0300

    Finished ValueRef smart pointer

--- vaca-core/src/value/valueref.rs
+++ vaca-core/src/value/valueref.rs
@@ -2,6 +2,7 @@ use std::ops::Deref;
 
 use crate::Value;
 
+#[derive(Debug)]
 /// ValueRef is the type that Vaca uses to share memory and values across the program using ownership
 /// Values can be owned in the scope or owned by the SymbolTable, which returns ValueRef as a *const Value
 pub enum ValueRef {
@@ -16,8 +17,22 @@ impl ValueRef {
     }
 
     /// Takes the reference, converts it to a *const Value and stores it
-    pub fn point(other: &Value) -> Self {
-        Self::Pointed(other as *const Value)
+    pub fn point(other: &Self) -> Self {
+        let ptr = match other {
+            ValueRef::Owned(own) => own as *const Value,
+            ValueRef::Pointed(ptr) => *ptr,
+        };
+
+        Self::Pointed(ptr)
+    }
+
+    pub fn take(self) -> Value {
+        match self {
+            ValueRef::Owned(own) => own,
+            ValueRef::Pointed(ptr) => unsafe {
+                (*ptr).clone()
+            },
+        }
     }
 
     pub fn is_valid(&self) -> bool {
@@ -25,6 +40,18 @@ impl ValueRef {
             ValueRef::Owned(_) => true,
             ValueRef::Pointed(v) => !v.is_null(),
         }
+    }
+
+    pub fn is_owned(&self) -> bool {
+        if let Self::Owned(_) = &self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_pointed(&self) -> bool {
+        !self.is_owned()
     }
 }
 
@@ -36,6 +63,26 @@ impl Deref for ValueRef {
         match self {
             ValueRef::Owned(v) => v,
             ValueRef::Pointed(v) => unsafe { v.as_ref().unwrap() },
+        }
+    }
+}
+
+impl Clone for ValueRef {
+    fn clone(&self) -> Self {
+        match self {
+            Self::Owned(arg0) => Self::Owned(arg0.clone()),
+            Self::Pointed(arg0) => unsafe { Self::Owned((**arg0).clone()) },
+        }
+    }
+}
+
+impl AsRef<Value> for ValueRef {
+    fn as_ref(&self) -> &Value {
+        match self {
+            ValueRef::Owned(own) => own,
+            ValueRef::Pointed(ptr) => unsafe {
+                ptr.as_ref().unwrap()
+            },
         }
     }
 }
