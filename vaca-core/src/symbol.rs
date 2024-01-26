172bf5e6d478c162af5ce958c45b161580889df7
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-26 07:33:05 -0300

    Added helper for extracting the namespace from a symbol

--- vaca-core/src/symbol.rs
+++ vaca-core/src/symbol.rs
@@ -9,6 +9,14 @@ impl Symbol {
     pub fn add_namespace(self, ns: &str) -> Self {
         Self(format!("{}/{}", ns, self.0))
     }
+
+    /// Takes a symbol and splits it into it's namespace and symbol
+    pub fn split_namespace(self) -> (Self, Self) {
+        let mut parts = self.0.splitn(2, "/");
+        let ns = parts.next().unwrap_or("");
+        let symbol = parts.next().unwrap_or("");
+        (Self::from(ns), Self::from(symbol))
+    }
 }
 
 impl From<&'_ str> for Symbol {
