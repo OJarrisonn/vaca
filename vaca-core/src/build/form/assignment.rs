5b59785ed1e5180a7be8781d66e900016b8e5d5a
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-29 16:06:32 -0300

    Removed final assignments

--- vaca-core/src/build/form/assignment.rs
+++ vaca-core/src/build/form/assignment.rs
@@ -5,10 +5,3 @@ use crate::build::symbol::Symbol;
 use super::Form;
 
 pub type Assignment = (Symbol, Form);
-
-#[derive(Debug, Clone, Readable, Writable)]
-pub enum AssignmentKind {
-    Immutable,
-    Mutable,
-    Final
-}
