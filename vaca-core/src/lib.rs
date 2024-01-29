2acd277a96902d361b2186fba550d8e759423672
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-29 11:22:03 -0300

    Setted new core modules

--- vaca-core/src/lib.rs
+++ vaca-core/src/lib.rs
@@ -1,15 +1,2 @@
-//mod form;
-pub mod value;
-mod symbol;
-mod table;
-pub mod form;
-mod error;
-#[macro_use]
-mod macros;
-
-pub use crate::{value::{Value, result::ExecResult}, symbol::Symbol, table::SymbolTable, form::Form, error::ErrorStack};
-
-#[cfg(test)]
-mod tests {
-    
-}
+pub mod run;
+pub mod build;
