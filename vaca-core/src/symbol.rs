be5490cac57939d1da21e8aa350d0ee0d0312662
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-29 07:13:30 -0300

    Refactored symbol to obey the new Symbol standard from the grammar

--- vaca-core/src/symbol.rs
+++ vaca-core/src/symbol.rs
@@ -3,28 +3,60 @@ use std::fmt::Display;
 use speedy::{Writable, Readable};
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Readable, Writable)]
-pub struct Symbol(String);
+/// Symbols are the name units of Vaca, all data association is made using symbols
+/// 
+/// `External` symbols can be obtained when renaming symbols imported by other libraries
+/// `Local` are the symbols created in the current Vaca scope
+pub enum Symbol {
+    External{ns: String, symbol: String},
+    Local(String)
+}
 
 impl Symbol {
-    pub fn add_namespace(self, ns: &str) -> Self {
-        Self(format!("{}/{}", ns, self.0))
+    /// Sets the namespace of a [`Symbol`], if it's `Local`, turns it into an `External`.
+    /// If already is an `External`, just replaces the `ns` field
+    pub fn export(self, namespace: String) -> Self {
+        match self {
+            Symbol::External { ns, symbol } => Symbol::External { ns: namespace, symbol },
+            Symbol::Local(symbol) => Symbol::External { ns: namespace, symbol },
+        }
+    }
+
+    /// Checks if the [`Symbol`] has the mutability signal `'` at the end
+    pub fn is_mutable(&self) -> bool {
+        match self {
+            Symbol::External { ns: _, symbol } | Symbol::Local(symbol) => symbol.ends_with("'"),
+        }
     }
 }
 
 impl From<&'_ str> for Symbol {
     fn from(value: &'_ str) -> Self {
-        Self(String::from(value))
+        if value.chars().next().unwrap().is_alphabetic() && value.contains("/") {
+            let value: Vec<&str> = value.splitn(2, "/").collect();
+            Self::External { ns: value[0].into(), symbol: value[1].into() }
+        } else {
+            Self::Local(value.into())
+        }
     }
 }
 
 impl From<String> for Symbol {
     fn from(value: String) -> Self {
-        Self(value)
+        if value.chars().next().unwrap().is_alphabetic() && value.contains("/") {
+            let value: Vec<&str> = value.splitn(2, "/").collect();
+            Self::External { ns: value[0].into(), symbol: value[1].into() }
+        } else {
+            Self::Local(value)
+        }
     }
 }
 
 impl Display for Symbol {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", &self.0)
+        match self {
+            Symbol::External { ns, symbol } => write!(f, "{}/{}", ns, symbol),
+            Symbol::Local(symbol) => write!(f, "{}", symbol),
+        }
     }
 }
