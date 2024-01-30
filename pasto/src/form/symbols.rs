1da34bce1672c0cb4f5e000d86e53a734d5b705f
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-30 10:05:26 -0300

    Moved to literals.rs

--- pasto/src/form/symbols.rs
+++
@@ -1,8 +0,0 @@
-use chifre::Rule;
-use pest::iterators::Pair;
-use vaca_core::build::symbol::Symbol;
-
-/// Receive any rule who inner is a symbol* and generates a vector of symbols
-pub fn parse_symbols(symbols: Pair<Rule>) -> Vec<Symbol> {
-    symbols.into_inner().map(|symbol| Symbol::from(symbol.as_span().as_str())).collect()
-}
