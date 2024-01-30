bae50cbab00d11f8fad4c4d150a0291f360284d7
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-30 10:04:11 -0300

    Added parsers for almost all forms

--- pasto/src/form.rs
+++ pasto/src/form.rs
@@ -1,16 +1,14 @@
-use std::error::Error;
-
 use chifre::Rule;
 use pest::iterators::Pair;
-use vaca_core::build::{error::BuildErrorStack, form::{Form, Span}};
+use vaca_core::build::form::{Form, Span};
 
 mod assignment;
 mod collections;
 mod callables;
-mod symbols;
+mod literals;
 
 /// Receives a [`Rule::form`] or any of its possible inners and creates a Form
-pub fn parse_form(form: Pair<Rule>) -> Form, BuildErrorStack> {
+pub fn parse_form(form: Pair<Rule>) -> Form {
     let span = Span::from(form.as_span());
 
     let expr = match form.as_rule() {
@@ -18,41 +16,22 @@ pub fn parse_form(form: Pair<Rule>) -> Form, BuildErrorStack> {
         Rule::array_list => collections::parse_array(form),
         Rule::scope => collections::parse_scope(form),
         Rule::function => callables::parse_function(form),
-        Rule::macrodef => todo!(),
-        Rule::call => todo!(),
-        Rule::infix_call => todo!(),
-        Rule::literal => todo!(),
+        Rule::macrodef => callables::parse_macro(form),
+        Rule::call => callables::parse_call(form),
+        Rule::infix_call => todo!(),//callables::parse_infix(form),
+        Rule::literal => literals::parse_literal(form),
         Rule::dontcare => todo!(),
         Rule::form => return parse_form(form),
         _ => unreachable!()
     };
 
-    match expr {
-        Ok(expr) => Ok(Form::new(span, expr)),
-        Err(err) => Err(BuildErrorStack::Stream { from: Box::new(err), src: span, note: None }),
-    }
+    Form::new(span, expr)
 
 }
 
-pub fn parse_forms(forms: Pair<Rule>) -> Result<Vec<Form>, BuildErrorStack> {
-    let span = forms.as_span().into();
-    
-    let (oks, mut errs): (Vec<_>, Vec<_>) = forms.into_inner()
+/// Receive a [`Rule::forms`] and generate a vector of each form parsed
+pub fn parse_forms(forms: Pair<Rule>) -> Vec<Form> {
+    forms.into_inner()
         .map(|form| parse_form(form.into_inner().next().unwrap()))
-        .partition(|form| form.is_ok());
-
-    if errs.is_empty() {
-        let forms = oks.into_iter().map(Result::unwrap).collect::<Vec<Form>>();
-        Ok(forms)
-    } else if errs.len() == 1 {
-        let err = BuildErrorStack::Stream { from: Box::new(errs.pop().unwrap().unwrap_err()), src: span, note: Some("A single form parsing failed in the current scope".into()) };
-        Err(err)
-    } else {
-        let count = errs.len();
-        let err = BuildErrorStack::MultiStream { 
-            from: errs.into_iter().map(|err| Box::new(err.unwrap_err()) as Box<dyn Error>).collect(), 
-            src: span, 
-            note: Some(format!("{} forms parsing failed in the current scope", count)) };
-        Err(err)
-    }
+        .collect()
 }
