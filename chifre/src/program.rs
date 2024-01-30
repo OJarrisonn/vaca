dc748e41b1aa9db0525b447f3f390ccbba556ca3
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-30 16:45:35 -0300

    Passing the Pair instead of it's inner let me collect their span later

--- chifre/src/program.rs
+++ chifre/src/program.rs
@@ -1,4 +1,4 @@
-use pest::iterators::Pairs;
+use pest::iterators::Pair;
 use pest::Parser;
 use vaca_core::build::error::LexError;
 
@@ -6,12 +6,12 @@ use crate::{Lexer, Rule};
 
 /// Receives some input source code and generates tokens for an entire program
 /// The output Ok variant has the imports in the .0, and the forms in the .1
-pub fn lex_program<'a>(source: &'a str) -> Result<(Pairs<'a, Rule>, Pairs<'a, Rule>), LexError> {
+pub fn lex_program<'a>(source: &'a str) -> Result<(Pair<'a, Rule>, Pair<'a, Rule>), LexError> {
     match Lexer::parse(Rule::program, source) {
         Ok(mut tokens) => {
             let mut program = tokens.next().unwrap().into_inner();
-            let imports = program.next().unwrap().into_inner();
-            let forms = program.next().unwrap().into_inner();
+            let imports = program.next().unwrap();
+            let forms = program.next().unwrap();
 
             Ok((imports, forms))
         },
