9bcdb9ddca5ca790565c01af7a91bcb236fdb3b2
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-30 10:04:59 -0300

    Removed result verification

--- pasto/src/form/assignment.rs
+++ pasto/src/form/assignment.rs
@@ -7,35 +7,20 @@ use vaca_core::build::{error::BuildErrorStack, form::{Expr, Form, Span}, symbol:
 use super::parse_form;
 
 /// Receives a [`Rule::assignments`] and turn it into an AssignmentList expression
-pub fn parse_assignment_list(assignments: Pair<Rule>) -> Result<Expr, BuildErrorStack> {
+pub fn parse_assignment_list(assignments: Pair<Rule>) -> Expr {
     let span = Span::from(assignments.as_span());
     let list = assignments.into_inner();
 
-    let (oks, mut errs): (Vec<_>, Vec<_>) = list.map(parse_assignment).partition(|res| res.is_ok());
-
-    // Checks for errors and create the proper BuildErrorStack if needed
-    if errs.is_empty() {
-        let assignments = oks.into_iter().map(Result::unwrap).collect::<Vec<(Symbol, Form)>>();
-        Ok(Expr::AssignmentList(assignments))
-    } else if errs.len() == 1 {
-        let err = BuildErrorStack::Stream { from: Box::new(errs.pop().unwrap().unwrap_err()), src: span, note: Some("A single assignment failed in the current assignment list".into()) };
-        Err(err)
-    } else {
-        let count = errs.len();
-        let err = BuildErrorStack::MultiStream { 
-            from: errs.into_iter().map(|err| Box::new(err.unwrap_err()) as Box<dyn Error>).collect(), 
-            src: span, 
-            note: Some(format!("{} assignments failed", count)) };
-        Err(err)
-    }
+    let assignments = list.map(parse_assignment).collect();
+    Expr::AssignmentList(assignments)
 }
 
 /// Receives an [`Rule::assignment`] and Extracts the Symbol and Form from it
-fn parse_assignment(token: Pair<Rule>) -> Result<(Symbol, Form), BuildErrorStack> {
+fn parse_assignment(token: Pair<Rule>) -> (Symbol, Form) {
     let span = Span::from(token.as_span());
     let mut iter = token.into_inner();
     let symbol = iter.next().unwrap().as_span().as_str().into();
-    let form = parse_form(iter.next().unwrap().into_inner().next().unwrap()).map_err(|err| BuildErrorStack::Stream { from: Box::new(err), src: span, note: Some("The parsing of the form passed to the current assignment failed".into()) })?;
+    let form = parse_form(iter.next().unwrap().into_inner().next().unwrap());
 
-    Ok((symbol, form))
+    (symbol, form)
 }
--- pasto/src/form/collections.rs
+++ pasto/src/form/collections.rs
@@ -1,23 +1,15 @@
 use chifre::Rule;
 use pest::iterators::Pair;
-use vaca_core::build::{error::BuildErrorStack, form::Expr};
+use vaca_core::build::form::Expr;
 
 use super::parse_forms;
 
 /// Receives a [`Rule::array_list`] and turns it into an [`Expr::Array`]
-pub fn parse_array(array: Pair<Rule>) -> Result<Expr, BuildErrorStack> {
-    let span = array.as_span().into();
-
-    parse_forms(array.into_inner().next().unwrap()) // The inner of array is forms
-        .map(|ok| Expr::Array(ok))
-        .map_err(|err| BuildErrorStack::Stream { from: Box::new(err), src: span, note: None })
+pub fn parse_array(array: Pair<Rule>) -> Expr {
+    Expr::Array(parse_forms(array.into_inner().next().unwrap())) // The inner of array is forms
 }
 
 /// Receives a [`Rule::scope`] and turns it into an [`Expr::Array`]
-pub fn parse_array(scope: Pair<Rule>) -> Result<Expr, BuildErrorStack> {
-    let span = scope.as_span().into();
-
-    parse_forms(array.into_inner().next().unwrap()) // The inner of array is forms
-        .map(|ok| Expr::Array(ok))
-        .map_err(|err| BuildErrorStack::Stream { from: Box::new(err), src: span, note: None })
+pub fn parse_scope(scope: Pair<Rule>) -> Expr {
+    Expr::Scope(parse_forms(scope.into_inner().next().unwrap())) // The inner of array is forms
 }
