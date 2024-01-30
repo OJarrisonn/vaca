e0de1098550b303ac7190a1f20b88822ea93270c
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-29 23:25:44 -0300

    Added simple LexError

--- vaca-core/src/build/error.rs
+++ vaca-core/src/build/error.rs
@@ -1,3 +1,25 @@
-pub struct BuildError {
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug)]
+pub struct LexError(pub String);
+
+#[derive(Debug)]
+pub struct BuildErrorStack {
     
+}
+
+impl Error for LexError {}
+
+impl Display for LexError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for BuildErrorStack {}
+
+impl Display for BuildErrorStack {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "LexError")
+    }
 }
