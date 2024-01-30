c702ee59c6ba071b2408c0fcbc24ab09f5848984
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-01-30 10:13:36 -0300

    Fixed warnings

--- pasto/src/form/assignment.rs
+++ pasto/src/form/assignment.rs
@@ -1,14 +1,11 @@
-use std::error::Error;
-
 use chifre::Rule;
 use pest::iterators::Pair;
-use vaca_core::build::{error::BuildErrorStack, form::{Expr, Form, Span}, symbol::Symbol};
+use vaca_core::build::{form::{Expr, Form}, symbol::Symbol};
 
 use super::parse_form;
 
 /// Receives a [`Rule::assignments`] and turn it into an AssignmentList expression
 pub fn parse_assignment_list(assignments: Pair<Rule>) -> Expr {
-    let span = Span::from(assignments.as_span());
     let list = assignments.into_inner();
 
     let assignments = list.map(parse_assignment).collect();
@@ -17,7 +14,6 @@ pub fn parse_assignment_list(assignments: Pair<Rule>) -> Expr {
 
 /// Receives an [`Rule::assignment`] and Extracts the Symbol and Form from it
 fn parse_assignment(token: Pair<Rule>) -> (Symbol, Form) {
-    let span = Span::from(token.as_span());
     let mut iter = token.into_inner();
     let symbol = iter.next().unwrap().as_span().as_str().into();
     let form = parse_form(iter.next().unwrap().into_inner().next().unwrap());
