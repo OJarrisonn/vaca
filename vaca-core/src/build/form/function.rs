e2fdee25c6bd1849198cd30c1ad1140f69bf8cc2
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-02-01 08:51:16 -0300

    Functions doesn't need captures since known symbols are resolved into their values during build

--- vaca-core/src/build/form/function.rs
+++ vaca-core/src/build/form/function.rs
@@ -7,22 +7,16 @@ use super::Form;
 #[derive(Debug, Clone, Readable, Writable)]
 
 pub struct Function {
-    captures: Option<Vec<Symbol>>, 
     parameters: Vec<Symbol>, 
     body: Box<Form>
 }
 
 impl Function {
-    pub fn new(captures: Option<Vec<Symbol>>, parameters: Option<Vec<Symbol>>, body: Form) -> Self {
+    pub fn new(parameters: Option<Vec<Symbol>>, body: Form) -> Self {
         Self {
-            captures, 
             parameters: parameters.unwrap_or_default(),
             body: Box::new(body)
         }
-    }
-
-    pub fn captures(&self) -> &Option<Vec<Symbol>> {
-        &self.captures
     }
 
     pub fn parameters(&self) -> &Vec<Symbol> {
--- vaca-core/src/run/value/function.rs
+++ vaca-core/src/run/value/function.rs
@@ -41,34 +41,6 @@ impl Function {
         self.arity
     }
 
-    /*pub fn exec(&self, source_args: Array, table: Arc<RwLock<SymbolTable>>) -> Result<ValueRef, RunErrorStack> {
-        if self.arity < source_args.len() {
-            return Err(RunErrorStack::Top { 
-                src: Some(format!("{} ->", self.span_from_args())), 
-                msg: format!("Too many arguments passed to function call, expected {}, but got {}", self.arity, source_args.len()) 
-            });
-        } else if self.arity > source_args.len() {
-            return Ok(ValueRef::own(Value::Function(Function::partial(&self, source_args))));
-        }
-
-        let mut args = self.partials.clone();
-        args.extend(source_args.into_iter());
-        
-        table.create_scope();
-
-        zip(&self.params, args).for_each(|(s, v)| table.register(s.clone(), v.take()));
-
-        let res = if self.body.is_none() {
-            self.native.unwrap()(table)
-        } else {
-            self.body.as_ref().unwrap().eval(table)
-        };
-
-        table.drop_scope();
-
-        return res;
-    }*/
-
     pub fn partial(source: &Self, args: Array) -> Self {
         let mut source = source.clone();
         source.arity -= args.len();
