bb4838036f0e9da1923cfc68d6e1f90c272e26f4
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-02-01 10:03:39 -0300

    Added captures to macros

--- vaca-core/src/build/form/macros.rs
+++ vaca-core/src/build/form/macros.rs
@@ -6,15 +6,29 @@ use crate::build::symbol::Symbol;
 use super::Form;
 #[derive(Debug, Clone, Readable, Writable)]
 pub struct Macro {
+    captures: Option<Vec<Symbol>>, 
     parameters: Vec<Symbol>, 
     body: Box<Form>
 }
 
 impl Macro {
-    pub fn new(parameters: Option<Vec<Symbol>>, body: Form) -> Self {
+    pub fn new(captures: Option<Vec<Symbol>>, parameters: Option<Vec<Symbol>>, body: Form) -> Self {
         Self {
+            captures,
             parameters: parameters.unwrap_or_default(), 
             body: Box::new(body)
         }
+    }
+
+    pub fn captures(&self) -> &Option<Vec<Symbol>> {
+        &self.captures
+    }
+
+    pub fn parameters(&self) -> &Vec<Symbol> {
+        &self.parameters
+    }
+
+    pub fn body(&self) -> &Box<Form> {
+        &self.body
     }
 }
