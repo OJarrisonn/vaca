f944d1ef86cc62b4a526a3169ad050cc927103b8
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-02-01 09:55:02 -0300

    Added helper for checking if an symbol is an action

--- vaca-core/src/run/table.rs
+++ vaca-core/src/run/table.rs
@@ -9,8 +9,14 @@ use super::{error::RunErrorStack, result::RunResult, value::Value, valueref::Val
 #[derive(Debug)]
 pub struct SymbolTable {
     parent: Option<Arc<RwLock<Self>>>,
-    scope: FxHashMap<Symbol, ValueRef>,
+    scope: FxHashMap<Symbol, SymbolTableEntry>,
     mutables: FxHashMap<Symbol, ValueRef>
+}
+
+#[derive(Debug)]
+pub struct SymbolTableEntry {
+    value: ValueRef,
+    is_action: bool
 }
 
 impl SymbolTable {
@@ -34,15 +40,15 @@ impl SymbolTable {
         Arc::new(RwLock::new(child))
     }
 
-    pub fn lookup(&self, symbol: Symbol) -> RunResult<ValueRef> {
+    pub fn lookup(&self, symbol: &Symbol) -> RunResult<ValueRef> {
         if symbol.is_mutable() {
-            match self.mutables.get(&symbol) {
+            match self.mutables.get(symbol) {
                 Some(value) => Ok(ValueRef::clone(value)),
                 None => Err(RunErrorStack::Top { src: Some(symbol.to_string()), msg: format!("use of undefined mutable symbol `{}`. Mutable symbols are only accessible in the scope they were created", symbol) }),
             }
         } else {
-            match self.scope.get(&symbol) {
-                Some(value) => Ok(ValueRef::clone(value)),
+            match self.scope.get(symbol) {
+                Some(value) => Ok(ValueRef::clone(&value.value)),
                 None => match &self.parent {
                     Some(parent) => parent.read()
                         .map_err(|err| RunErrorStack::Stream { 
@@ -56,7 +62,26 @@ impl SymbolTable {
         }
     }
 
-    pub fn assign(&mut self, symbol: Symbol, value: Value) -> RunResult<()> {
+    pub fn is_action(&self, symbol: &Symbol) -> RunResult<bool> {
+        if symbol.is_mutable() {
+            Ok(true)
+        } else {
+            match self.scope.get(symbol) {
+                Some(value) => Ok(value.is_action),
+                None => match &self.parent {
+                    Some(parent) => parent.read()
+                        .map_err(|err| RunErrorStack::Stream { 
+                            src: Some(symbol.to_string()), 
+                            from: Box::new(RunErrorStack::Top { src: None, msg: err.to_string() }), 
+                            note: Some(format!("failed to access parent scope while tring to get the value of `{}`", symbol)) 
+                        })?.is_action(symbol),
+                    None => Err(RunErrorStack::Top { src: Some(symbol.to_string()), msg: format!("use of undefined symbol `{}`", symbol) }),
+                },
+            }
+        }
+    }
+
+    pub fn assign(&mut self, symbol: Symbol, value: Value, is_action: bool) -> RunResult<()> {
         if symbol.is_mutable() {
             self.mutables.insert(symbol, ValueRef::new(value));
             Ok(())
@@ -64,7 +89,7 @@ impl SymbolTable {
             match self.scope.get(&symbol) {
                 Some(_) => Err(RunErrorStack::Top { src: Some(symbol.to_string()), msg: format!("atempt to mutate immutable symbol `{}`. If you need mutation, try creating `{}'`", symbol, symbol) }),
                 _ => {
-                    self.scope.insert(symbol, ValueRef::new(value));
+                    self.scope.insert(symbol, SymbolTableEntry { value: ValueRef::new(value), is_action });
                     Ok(())
                 }
             }
