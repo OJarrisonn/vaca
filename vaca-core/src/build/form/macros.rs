041cd80542ce15846546e1a876a2e7767d454b87
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-02-01 13:21:44 -0300

    Removed captures They'll be defined in the runtime

--- vaca-core/src/build/form/macros.rs
+++ vaca-core/src/build/form/macros.rs
@@ -6,22 +6,16 @@ use crate::build::symbol::Symbol;
 use super::Form;
 #[derive(Debug, Clone, Readable, Writable)]
 pub struct Macro {
-    captures: Option<Vec<Symbol>>, 
     parameters: Vec<Symbol>, 
     body: Box<Form>
 }
 
 impl Macro {
-    pub fn new(captures: Option<Vec<Symbol>>, parameters: Option<Vec<Symbol>>, body: Form) -> Self {
+    pub fn new(parameters: Option<Vec<Symbol>>, body: Form) -> Self {
         Self {
-            captures,
             parameters: parameters.unwrap_or_default(), 
             body: Box::new(body)
         }
-    }
-
-    pub fn captures(&self) -> &Option<Vec<Symbol>> {
-        &self.captures
     }
 
     pub fn parameters(&self) -> &Vec<Symbol> {
