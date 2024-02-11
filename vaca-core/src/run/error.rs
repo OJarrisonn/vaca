81e25c8e11f28f8d310b17618b16b4360a479976
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-02-11 12:09:40 -0300

    Created runtime error

--- vaca-core/src/run/error.rs
+++ vaca-core/src/run/error.rs
@@ -1,5 +1,7 @@
 use std::{error::Error, fmt::Display};
 
+use super::result::RunResult;
+
 //#[derive(Debug)]
 //pub struct GenericError(pub String);
 
@@ -9,7 +11,8 @@ use std::{error::Error, fmt::Display};
 /// When not at the top of the stack you may reference the Error, which can be anything that impl std::error::Error
 pub enum RunErrorStack {
     Top{src: Option<String>, msg: String},
-    Stream{src: Option<String>, from: Box<dyn std::error::Error>, note: Option<String>}
+    Stream{from: Box<dyn std::error::Error>, src: Option<String>, note: Option<String>},
+    MultiStream{from: Vec<Box<dyn Error>>, src: String, note: Option<String>}
 }
 
 //impl Error for GenericError {}
@@ -18,6 +21,18 @@ pub enum RunErrorStack {
 //     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 //         write!(f, "Error: {}", self.0)
 //     }
+
+impl RunErrorStack {
+    pub fn into_result<T>(mut errs: Vec<Self>, ok: T) -> RunResult<T> {
+        if errs.is_empty() {
+            Ok(ok)
+        } else if errs.len() == 1 {
+            Err(errs.pop().unwrap())
+        } else {
+            Err(Self::MultiStream { from: errs.into_iter().map(|err| Box::new(err) as Box<dyn Error>).collect(), src: "program".into(), note: None })
+        }
+    }
+}
 
 impl From<String> for RunErrorStack {
     fn from(value: String) -> Self {
@@ -53,6 +68,17 @@ impl Display for RunErrorStack {
                     Ok(())
                 }
             },
+            RunErrorStack::MultiStream { from, src, note } => {
+                writeln!(f, "Multiple Error Stream caused by: {src}")?;
+                for err in from.iter() {
+                    writeln!(f, "{err}")?;
+                }
+                match note {
+                    Some(note) => writeln!(f, "{note}")?,
+                    None => (),
+                }
+                write!(f, "End of Error Stream caused")
+            }
         }
     }
 }
